eaa5fbd74fe4687e3ddbbbba715e31d91e27b4af
Anujmishra14 <anuj14112000@gmail.com>
2026-03-22 21:10:01 +0530

    Updated

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const mongoUri=process.env.MONGO_URI;
 const app = express();
 
 app.use(cors({
-  origin: "https://contact-manager-a.netlify.app/",
+  origin: "https://contact-manager-a.netlify.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
@@ -27,3 +27,5 @@ app.get("/", (req, res) => {
 app.use("/api/contacts", require("./routes/contactRoutes"));
 const PORT=process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
