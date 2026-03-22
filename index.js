12cd2ffe8b5ce4f8aae6e8ee01b9ff452a7fffa4
Anujmishra14 <anuj14112000@gmail.com>
2026-03-22 21:11:24 +0530

    Updated

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ app.get("/", (req, res) => {
 
 // routes
 app.use("/api/contacts", require("./routes/contactRoutes"));
+
 const PORT=process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
